5f3cd0ce5a88c9581ef4532c0923c6e75aa27e02
praveen <jbapraveen@hotmail.com>
2017-09-02 23:18:59 +0530

    Initial Commit

---
+++ script/script.js
@@ -0,0 +1,80 @@
+$(document).ready(function() {
+	$('#fullpage').fullpage({
+		//Navigation
+		menu: '#menu',
+		lockAnchors: false,
+		anchors:['firstPage'],
+		navigation: true,
+		navigationPosition: 'right',
+		navigationTooltips: ['Home', 'About','Portfolio', 'Vlog', 'Contact'],
+		showActiveTooltip: true,
+		slidesNavigation: true,
+		slidesNavPosition: 'bottom',
+		//Scrolling
+		css3: true,
+		scrollingSpeed: 800,
+		autoScrolling: true,
+		fitToSection: true,
+		fitToSectionDelay: 1000,
+		scrollBar: false,
+		easing: 'easeInOutCubic',
+		easingcss3: 'ease',
+		loopBottom: false,
+		loopTop: false,
+		loopHorizontal: true,
+		continuousVertical: false,
+		continuousHorizontal: false,
+		scrollHorizontally: false,
+		interlockedSlides: false,
+		dragAndMove: false,
+		offsetSections: false,
+		resetSliders: false,
+		fadingEffect: false,
+		normalScrollElements: '#element1, .element2',
+		scrollOverflow: false,
+		scrollOverflowReset: false,
+		scrollOverflowOptions: null,
+		touchSensitivity: 15,
+		normalScrollElementTouchThreshold: 5,
+		bigSectionsDestination: null,
+		//Accessibility
+		keyboardScrolling: true,
+		animateAnchor: true,
+		recordHistory: true,
+		//Design
+		controlArrows: false,
+		verticalCentered: true,
+		sectionsColor : ['#000','#000','#000','#000','#000'],
+		paddingTop: '3em',
+		paddingBottom: '10px',
+		fixedElements: '#header, .footer',
+		responsiveWidth: 0,
+		responsiveHeight: 0,
+		responsiveSlides: true,
+		parallax: true,
+		parallaxOptions: {type: 'reveal', percentage: 62, property: 'translate'},
+		//Custom selectors
+		sectionSelector: '.section',
+		slideSelector: '.slide',
+		lazyLoading: true,
+		//events
+		onLeave: function(index, nextIndex, direction){},
+		afterLoad: function(anchorLink, index){},
+		afterRender: function(){
+			// $( '#footer' ).css( { 
+			// 	'height' : 200 
+			// } );
+
+		},
+		afterResize: function(){},
+		afterResponsive: function(isResponsive){},
+		afterSlideLoad: function(anchorLink, index, slideAnchor, slideIndex){},
+		onSlideLeave: function(anchorLink, index, slideIndex, direction, nextSlideIndex){}
+	});
+});
+
+$(document).on('click', '.thumbnail', function(event){
+
+	$('.portfolio-container').html('<img src="' + event.target.src + '">');
+	
+});
